a3baae1f428a65fd11771790695dda3901bed422
ChalitoDavid <94726685+ChalitoDavid@users.noreply.github.com>
2021-12-02 21:15:47 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var starImg,bgImg;
+var star, starBody;
+var fairy, fairyImg;
+//crea la variable para el sprite del hada y fairyImg
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	//escribe el código para reproducir el sonido fairyVoice
+
+	//crea el sprite del hada, y agrega la animación para el hada
+    fairy = createSprite(width/2, height/2, 10, 10)
+	fairy.addAnimation("fairyImg1", fairyImg);
+    fairy.scale = .2;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  //escribe aquí el código para detener la estrella en la mano del hada
+
+  if(fairy.y - star.y < fairy.height/2 + star.height/2){
+	  Matter.Body.setStatic(starBody, true); 
+  }
+
+   console.log(fairy.y - star.y) 
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//escribe el código para mover al hada a la izquierda y derecha
+	
+	if (keyCode === LEFT_ARROW) {
+
+		fairy.x = fairy.x -10
+
+	}
+
+	if (keyCode === RIGHT_ARROW){
+
+		fairy.x = fairy.x +10
+	}
+}
